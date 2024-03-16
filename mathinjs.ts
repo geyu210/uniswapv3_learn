7ab8862e663a8629f247616cbb6b2049eaa77644
geyu <geyu210@gmail.com>
2024-03-16 13:17:34 +0800

    chore: forge init

---
+++ mathinjs.ts
@@ -0,0 +1,24 @@
+const q96 = 2**96
+function getBaseLog(x:number, y:number) {
+    return (Math.log(y) / Math.log(x)).toFixed();
+}
+
+let tick_low = getBaseLog(1.0001,4545)
+console.log(tick_low)
+let tick_now = getBaseLog(1.0001,5000)
+console.log(tick_now)
+let tick_high = getBaseLog(1.0001,5500)
+console.log(tick_high)
+function pricetotick(p:number){
+    return getBaseLog(1.0001,p)
+}
+console.log(pricetotick(4545))
+console.log(pricetotick(5000))
+console.log(pricetotick(5500))
+
+function price_to_sqrtp(p:number){
+    return (Math.sqrt(p)*q96).toFixed()
+}
+
+console.log(price_to_sqrtp(5000))
+
